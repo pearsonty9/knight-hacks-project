abd4aee3a24e2402a208264fe55c93da93c5e655
Tyler Pearson <pearsonty9@gmail.com>
2020-10-10 19:16:11 -0400

    Added UI elements to index.html

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //AIzaSyCj7xxZkXQUQ3j3Ha8EfQKqsKuXJgA21g4
 
 let map;
+let markersArray = [];
+
+
+const radiusMult = 100;
+
+let radius = 1;
 
 function initMap() {
   // Configure the click listener.
@@ -28,14 +34,14 @@ function initMap() {
     console.log(origin);
     let request = {
       location: origin,
-      radius: '1000',
+      radius: '10000',
       type: ['park']
     };
 
     placesService.nearbySearch(request, (results, status) =>{
       if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (let i = 0; i < results.length; i++) {
-          var marker = new google.maps.Marker({
+          new google.maps.Marker({
             map: map,
             place: {
               placeId: results[i].place_id,
@@ -48,7 +54,7 @@ function initMap() {
         }
       }
     });
-    
+
     new google.maps.Marker({
       map: map,
       position: origin
@@ -57,9 +63,36 @@ function initMap() {
   });
 }
 
+function addMarker(latLng, color){
+  let url = "http://maps.google.com/mapfiles/ms/icons/";
+  url += color + "-dot.png";
+
+  let marker = new google.maps.Marker({
+    map: map,
+    position: latLng,
+    icon: {
+      url: url
+    }
+  });
+
+  //store the marker object drawn in global array
+  markersArray.push(marker);
+}
+
 function deleteMarkers(markersArray) {
   for (let i = 0; i < markersArray.length; i++) {
     markersArray[i].setMap(null);
   }
   markersArray = [];
 }
+
+
+let slider = document.getElementById("radiusSlider");
+let radiusOutput = document.getElementById("radiusValue");
+radiusOutput.innerHTML = slider.value;
+
+slider.oninput = () => {
+  radius = (slider.value * radiusMult) / 1000;
+  radiusOutput.innerHTML = radius;
+}
+console.log(radius);
