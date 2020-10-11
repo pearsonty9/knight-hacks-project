fc96614544082b39a25d5b3410ef777258e4af6c
Tyler Pearson <pearsonty9@gmail.com>
2020-10-11 00:25:40 -0400

    finished location finding and outputs

--- app.js
+++ app.js
@@ -9,102 +9,98 @@ let color;
 let radius = 1;
 
 function initMap() {
-    // Configure the click listener.
+  // Configure the click listener.
 
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-            lat: 37.6872,
-            lng: -97.3301
-        },
-        zoom: 5,
-    });
-    const placesService = new google.maps.places.PlacesService(map);
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 37.6872,
+      lng: -97.3301
+    },
+    zoom: 5,
+  });
+  const placesService = new google.maps.places.PlacesService(map);
 
-    let origin = {
-        lat: 0,
-        lng: 0
+  let origin = {
+    lat: 0,
+    lng: 0
+  };
+
+  let locationsData = [];
+
+  let ratingDiv = document.getElementById("rating-output");
+  let outputDiv = document.getElementById("output");
+
+  // Configure the click listener.
+  google.maps.event.addListener(map, 'click', (mapsMouseEvent) => {
+    origin = {
+      lat: mapsMouseEvent.latLng.lat(),
+      lng: mapsMouseEvent.latLng.lng()
     };
+    console.log(origin);
 
-    // Configure the click listener.
-    google.maps.event.addListener(map, 'click', (mapsMouseEvent) => {
-        origin = {
-            lat: mapsMouseEvent.latLng.lat(),
-            lng: mapsMouseEvent.latLng.lng()
-        };
-        console.log(origin);
-        let typeString = "";
-        for (let i = 0; i < searchType.length; i++) {
-            typeString += searchType[i] + ", ";
+    outputDiv.innerHTML = "";
+    deleteMarkers(markersArray);
+
+    markersArray.push(new google.maps.Marker({
+      map: map,
+      position: origin,
+    }));
+    let counter = 0;
+    for (let i = 0; i < searchType.length; ++i) {
+      //outputDiv += searchType[i] + ": " +  "<br>";
+      let request = {
+        location: origin,
+        radius: radius * 1000,
+        type: searchType[i]
+      };
+      console.log("SearhType: " + searchType[i]);
+      placesService.nearbySearch(request, (results, status) => {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          locationsData = results;
+          for (let i = 0; i < results.length; i++) {
+            markersArray.push(new google.maps.Marker({
+              map: map,
+              place: {
+                placeId: results[i].place_id,
+                location: results[i].geometry.location
+              },
+              icon: {
+                url: "http://maps.google.com/mapfiles/ms/icons/" + color + "-dot.png"
+              }
+            }));
+            if (results[i].name != null) {
+              outputDiv.innerHTML += "Name: " + results[i].name + "<br>";
+            } else {
+              outputDiv.innerHTML += "Name: not available" + "<br>";
+            }
+            if (results[i].vicinity != null) {
+              outputDiv.innerHTML += "Address: " + results[i].vicinity + "<br>";
+            } else {
+              outputDiv.innerHTML += "Address: not available" + "<br>";
+            }
+            if (results[i].rating != null) {
+              outputDiv.innerHTML += "Rating: " + results[i].rating + "<br>" + "<br>";
+            } else {
+              outputDiv.innerHTML += "Rating: not available" + "<br>" + "<br>";
+            }
+          }
         }
 
-        //console.log(typeString);
-        for (let i = 0; i < searchType.length; ++i) {
-            let request = {
-                location: origin,
-                radius: radius * 1000,
-                type: searchType[i]
-                
-            };
-            console.log(searchType[i]);
-            placesService.nearbySearch(request, (results, status) => {
-                if (status == google.maps.places.PlacesServiceStatus.OK) {
-                    let outputDiv = document.getElementById("output");
-                    outputDiv.innerHTML = "";
-                    let ratingDiv = document.getElementById("rating-output");
+      });
+      if(counter > Math.round(20 / searchType.length) - 1){
+        counter = 0;
+        continue;
+      } else{
+        counter++;
+      }
+    }
+    let finalRating = algorithm(locationsData, radius).toFixed(1);
+    ratingDiv.innerHTML = finalRating;
+    console.log("final rating: " + finalRating);
 
-                    deleteMarkers(markersArray);
-
-                    markersArray.push(new google.maps.Marker({
-                        map: map,
-                        position: origin,
-                    }));
-
-                    for (let i = 0; i < results.length; i++) {
-                        markersArray.push(new google.maps.Marker({
-                            map: map,
-                            place: {
-                                placeId: results[i].place_id,
-                                location: results[i].geometry.location
-                            },
-                            icon: {
-                                url: "http://maps.google.com/mapfiles/ms/icons/" + color + "-dot.png"
-                            }
-                        }));
-                        if (results[i].name != null) {
-                            outputDiv.innerHTML += "Name: " + results[i].name + "<br>";
-                        } else {
-                            "Name: not available" + "<br>";
-                        }
-                        if (results[i].vicinity != null) {
-                            outputDiv.innerHTML += "Address: " + results[i].vicinity + "<br>";
-                        } else {
-                            "Address: not available" + "<br>";
-                        }
-                        if (results[i].rating != null) {
-                            outputDiv.innerHTML += "Rating: " + results[i].rating + "<br>" + "<br>";
-                        } else {
-                            "Rating: not available" + "<br>";
-                        }
-                    }
-                    let finalRating = algorithm(results, radius).toFixed(1);
-                    ratingDiv.innerHTML = finalRating;
-                    console.log("final rating: " + finalRating);
-                }
-
-            });
-        }
-        });
+  });
 }
 
-/*function searchTypes(){
-  if(){
-    searchtype = 'park';
-  } else if(){
-    searchtype = 'fire_station'
-  } else if(){
-    searchtype = 'police';
-  }
-}*/
 
 function addMarker(latLng, color) {
   let url = "http://maps.google.com/mapfiles/ms/icons/";
@@ -130,57 +126,55 @@ function deleteMarkers(markersArray) {
 }
 
 function algorithm(resultsArray, radius) {
-    var qualityParks = 0;
-    var rating = 0;
-    if (resultsArray.length == 20 && radius > 3) {
-        rating = 7.2;
-    }
-    else if (resultsArray.length == 0 || resultsArray.length == null) {
-        rating = 0;
-        return rating;
-    }
-    else {
-        rating = (resultsArray.length / (Math.pow((radius), 2) * 3.14)) * 4;
-    }
-    console.log("num :" + rating);
-    for (let i = 0; i < resultsArray.length; ++i) {
-        if (resultsArray[i].rating >= 4.5 && resultsArray[i].rating != null) {
-            qualityParks = qualityParks + 1;
-            //console.log(qualityParks);
-        }
-        if (resultsArray[i].rating < 3.5 && resultsArray[i].rating != null) {
-            qualityParks = qualityParks - 1;
-        }
-    }
-    rating = rating + (3 * qualityParks / resultsArray.length);
-    if (rating < 0) {
-        rating = 0;
-    }
-    if (rating > 10) {
-        rating = 10;
-    }
-    //console.log(qualityParks / resultsArray.length);
+  var qualityParks = 0;
+  var rating = 0;
+  if (resultsArray.length == 20 && radius > 3) {
+    rating = 7.2;
+  } else if (resultsArray.length == 0 || resultsArray.length == null) {
+    rating = 0;
     return rating;
+  } else {
+    rating = (resultsArray.length / (Math.pow((radius), 2) * 3.14)) * 4;
+  }
+  console.log("num :" + rating);
+  for (let i = 0; i < resultsArray.length; ++i) {
+    if (resultsArray[i].rating >= 4.5 && resultsArray[i].rating != null) {
+      qualityParks = qualityParks + 1;
+      //console.log(qualityParks);
+    }
+    if (resultsArray[i].rating < 3.5 && resultsArray[i].rating != null) {
+      qualityParks = qualityParks - 1;
+    }
+  }
+  rating = rating + (3 * qualityParks / resultsArray.length);
+  if (rating < 0) {
+    rating = 0;
+  }
+  if (rating > 10) {
+    rating = 10;
+  }
+  //console.log(qualityParks / resultsArray.length);
+  return rating;
 }
 
-function radioChange(){
-  if(document.getElementById("parks").checked){
+function radioChange() {
+  if (document.getElementById("parks").checked) {
     searchType = ['park'];
     color = 'green';
     console.log(color);
-  } else if(document.getElementById("entertainment").checked){
+  } else if (document.getElementById("entertainment").checked) {
     searchType = ['shopping_mall', 'bowling_alley', 'movie_theater'];
     color = 'purple';
     console.log(color);
-  } else if(document.getElementById("public-services").checked){
+  } else if (document.getElementById("public-services").checked) {
     searchType = ['fire_station', 'police', 'hospital'];
     color = 'yellow';
     console.log(color);
-  } else if(document.getElementById("transportation").checked){
+  } else if (document.getElementById("transportation").checked) {
     searchType = ['train_station', 'transit_station', 'subway_station', 'bus_station'];
     color = 'blue';
     console.log(color);
-  } else if(document.getElementById("attractions").checked){
+  } else if (document.getElementById("attractions").checked) {
     searchType = ['amusement_park', 'zoo', 'museum', 'aquarium', 'art_gallery'];
     color = 'orange';
     console.log(color);
@@ -197,5 +191,3 @@ slider.oninput = () => {
   radiusOutput.innerHTML = radius;
 }
 console.log(radius);
-
-
