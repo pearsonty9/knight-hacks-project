ce4475834d499bda02935ed4a7fdda192820e8c5
dlew64 <daniel.lewis.fl@gmail.com>
2020-10-10 23:54:24 -0400

    for loop implementation

--- app.js
+++ app.js
@@ -3,96 +3,97 @@
 let map;
 let markersArray = [];
 
-let searchType;
+let searchType = [];
 let color;
 
 let radius = 1;
 
 function initMap() {
-  // Configure the click listener.
+    // Configure the click listener.
 
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: 37.6872,
-      lng: -97.3301
-    },
-    zoom: 5,
-  });
-  const placesService = new google.maps.places.PlacesService(map);
-
-  let origin = {
-    lat: 0,
-    lng: 0
-  };
-
-  // Configure the click listener.
-  google.maps.event.addListener(map, 'click', (mapsMouseEvent) => {
-    origin = {
-      lat: mapsMouseEvent.latLng.lat(),
-      lng: mapsMouseEvent.latLng.lng()
-    };
-    console.log(origin);
-    let typeString = "";
-    for(let i = 0; i < searchType.length; i++){
-      typeString += searchType[i] + ", ";
-    }
-
-    console.log(typeString);
-
-    let request = {
-      location: origin,
-      radius: radius * 1000,
-      type: searchType
-    };
-
-    placesService.nearbySearch(request, (results, status) => {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        let outputDiv = document.getElementById("output");
-        outputDiv.innerHTML = "";
-        let ratingDiv = document.getElementById("rating-output");
-        
-        deleteMarkers(markersArray);
-
-        markersArray.push(new google.maps.Marker({
-          map: map,
-          position: origin,
-        }));
-
-        for (let i = 0; i < results.length; i++) {
-          markersArray.push(new google.maps.Marker({
-            map: map,
-            place: {
-              placeId: results[i].place_id,
-              location: results[i].geometry.location
-            },
-            icon: {
-              url: "http://maps.google.com/mapfiles/ms/icons/" + color + "-dot.png"
-            }
-          }));
-          if (results[i].name != null) {
-            outputDiv.innerHTML += "Name: " + results[i].name + "<br>";
-          } else {
-            "Name: not available" + "<br>";
-          }
-          if (results[i].vicinity != null) {
-            outputDiv.innerHTML += "Address: " + results[i].vicinity + "<br>";
-          } else {
-            "Address: not available" + "<br>";
-          }
-          if (results[i].rating != null) {
-            outputDiv.innerHTML += "Rating: " + results[i].rating + "<br>" + "<br>";
-          } else {
-            "Rating: not available" + "<br>";
-          }
-        }
-        let finalRating = algorithm(results, radius).toFixed(1);
-        ratingDiv.innerHTML = finalRating;
-        console.log("final rating: " + finalRating);
-      }
-
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 37.6872,
+            lng: -97.3301
+        },
+        zoom: 5,
     });
+    const placesService = new google.maps.places.PlacesService(map);
 
-  });
+    let origin = {
+        lat: 0,
+        lng: 0
+    };
+
+    // Configure the click listener.
+    google.maps.event.addListener(map, 'click', (mapsMouseEvent) => {
+        origin = {
+            lat: mapsMouseEvent.latLng.lat(),
+            lng: mapsMouseEvent.latLng.lng()
+        };
+        console.log(origin);
+        let typeString = "";
+        for (let i = 0; i < searchType.length; i++) {
+            typeString += searchType[i] + ", ";
+        }
+
+        //console.log(typeString);
+        for (let i = 0; i < searchType.length; ++i) {
+            let request = {
+                location: origin,
+                radius: radius * 1000,
+                type: searchType[i]
+                
+            };
+            console.log(searchType[i]);
+            placesService.nearbySearch(request, (results, status) => {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    let outputDiv = document.getElementById("output");
+                    outputDiv.innerHTML = "";
+                    let ratingDiv = document.getElementById("rating-output");
+
+                    deleteMarkers(markersArray);
+
+                    markersArray.push(new google.maps.Marker({
+                        map: map,
+                        position: origin,
+                    }));
+
+                    for (let i = 0; i < results.length; i++) {
+                        markersArray.push(new google.maps.Marker({
+                            map: map,
+                            place: {
+                                placeId: results[i].place_id,
+                                location: results[i].geometry.location
+                            },
+                            icon: {
+                                url: "http://maps.google.com/mapfiles/ms/icons/" + color + "-dot.png"
+                            }
+                        }));
+                        if (results[i].name != null) {
+                            outputDiv.innerHTML += "Name: " + results[i].name + "<br>";
+                        } else {
+                            "Name: not available" + "<br>";
+                        }
+                        if (results[i].vicinity != null) {
+                            outputDiv.innerHTML += "Address: " + results[i].vicinity + "<br>";
+                        } else {
+                            "Address: not available" + "<br>";
+                        }
+                        if (results[i].rating != null) {
+                            outputDiv.innerHTML += "Rating: " + results[i].rating + "<br>" + "<br>";
+                        } else {
+                            "Rating: not available" + "<br>";
+                        }
+                    }
+                    let finalRating = algorithm(results, radius).toFixed(1);
+                    ratingDiv.innerHTML = finalRating;
+                    console.log("final rating: " + finalRating);
+                }
+
+            });
+        }
+        });
 }
 
 /*function searchTypes(){
@@ -129,28 +130,37 @@ function deleteMarkers(markersArray) {
 }
 
 function algorithm(resultsArray, radius) {
-  var qualityParks = 0;
-  //console.log("nums: " + resultsArray.length);
-  var rating = (resultsArray.length / (Math.pow((radius), 2) * 3.14)) * 5;
-  console.log("num :" + rating);
-  for (let i = 0; i < resultsArray.length; ++i) {
-    if (resultsArray[i].rating >= 4.5 && resultsArray[i].rating != null) {
-      qualityParks = qualityParks + 1;
-      //console.log(qualityParks);
+    var qualityParks = 0;
+    var rating = 0;
+    if (resultsArray.length == 20 && radius > 3) {
+        rating = 7.2;
     }
-    if (resultsArray[i].rating < 3.0 && resultsArray[i].rating != null) {
-      qualityParks = qualityParks + 1;
+    else if (resultsArray.length == 0 || resultsArray.length == null) {
+        rating = 0;
+        return rating;
     }
-  }
-  rating = rating + (3 * qualityParks / resultsArray.length);
-  if (rating < 0) {
-    rating = 0;
-  }
-  if (rating > 10) {
-    rating = 10;
-  }
-  //console.log(qualityParks / resultsArray.length);
-  return rating;
+    else {
+        rating = (resultsArray.length / (Math.pow((radius), 2) * 3.14)) * 4;
+    }
+    console.log("num :" + rating);
+    for (let i = 0; i < resultsArray.length; ++i) {
+        if (resultsArray[i].rating >= 4.5 && resultsArray[i].rating != null) {
+            qualityParks = qualityParks + 1;
+            //console.log(qualityParks);
+        }
+        if (resultsArray[i].rating < 3.5 && resultsArray[i].rating != null) {
+            qualityParks = qualityParks - 1;
+        }
+    }
+    rating = rating + (3 * qualityParks / resultsArray.length);
+    if (rating < 0) {
+        rating = 0;
+    }
+    if (rating > 10) {
+        rating = 10;
+    }
+    //console.log(qualityParks / resultsArray.length);
+    return rating;
 }
 
 function radioChange(){
@@ -187,3 +197,5 @@ slider.oninput = () => {
   radiusOutput.innerHTML = radius;
 }
 console.log(radius);
+
+
